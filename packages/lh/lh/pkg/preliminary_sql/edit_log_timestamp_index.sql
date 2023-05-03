9e49c6ad313e2b5bd03d7cc55ee27ab0c57d8f0b
glamcloud <thomas@wikman.org>
2023-05-03 13:14:26 +0200

    cleaned packages, no csv, empty package_config

---
+++ packages/lh/lh/pkg/preliminary_sql/edit_log_timestamp_index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX "edit_time_stamp"
+ON public.edit_log ("timestamp");
---
+++ packages/lh/lh/pkg/preliminary_sql/languages.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.d_languages(
+	languageid, languagename, isdefault)
+	VALUES ('nl', 'NEDERLANDS', true);
